96d514947f34c8d2915e5b82acf3fc430d9218e3
sangpham06112004 <sangpham06112004@gmail.com>
2025-12-20 22:32:31 +0700

    Add detail edit delete

--- src/features/users/routes/users.routes.js
+++ src/features/users/routes/users.routes.js
@@ -5,6 +5,10 @@ const { validateCreateUser } = require('../validators/users.validators');
 const { validationResult } = require('express-validator');
 
 router.get('/', userController.renderUserList);
+router.get('/:id/detail', userController.renderUserDetail);
+router.get('/:id/edit', userController.renderEditForm);
+router.post('/:id/edit', userController.updateUser);
+router.get('/:id/delete', userController.deleteUser);
 router.get('/create', userController.renderCreateForm);
 router.post('/create', validateCreateUser, userController.createUser);
 
